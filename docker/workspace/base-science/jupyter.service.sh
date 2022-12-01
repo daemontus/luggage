31cb602004194607450fff0cb63ec0249e28adee
Samuel Pastva <sam.pastva@gmail.com>
2022-12-01 18:29:55 +0100

    Try to force `jupyter` to use `bash` instead of `sh` as the default shell.

--- docker/workspace/base-science/jupyter.service.sh
+++ docker/workspace/base-science/jupyter.service.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Jupyter will not use the defulat shell of our user, but one specified in $SHELL.
+export SHELL=/bin/bash
 exec /sbin/setuser $WORKSPACE_USER jupyter lab --config=config.py >> /var/log/jupyter.log 2>&1
