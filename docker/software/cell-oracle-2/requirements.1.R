110cfef9adf2e198e4ef9d3178c96161f08b5008
Samuel Pastva <sam.pastva@gmail.com>
2023-06-16 19:20:29 +0200

    Actually working cell oracle.

---
+++ docker/software/cell-oracle-2/requirements.1.R
@@ -0,0 +1,5 @@
+BiocManager::install(c("Gviz", "GenomicRanges", "rtracklayer", "Rhtslib"))
+
+devtools::install_github("cole-trapnell-lab/cicero-release", ref = "monocle3")
+
+library(cicero)
---
+++ docker/software/cell-oracle-2/requirements.2.R
@@ -0,0 +1,6 @@
+
+# General packages for the jupyer labs R kernel.
+install.packages(c('repr','IRkernel','IRdisplay'))
+
+# Install kernel.
+IRkernel::installspec()
